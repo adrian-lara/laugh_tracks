08e40ae68a38aeefef83d95acf034181519104f3
Adrian Lara <adrianblara@gmail.com>
2017-10-06 10:34:06 -0600

    lots done. view and route created for comedian_index, model comedian has_many, avg age displayed

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,14 @@
+require './app/models/comedian'
+
 class LaughTracksApp < Sinatra::Base
+
+  get '/comedians' do
+    if params[:age]
+      @comedians = Comedian.where[age: params[:age]]
+    else
+      @comedians = Comedian.all
+    end
+    erb :comedian_index
+  end
+
 end
--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,9 @@
 class Comedian < ActiveRecord::Base
 
+  has_many :special
+
+  def self.avg_age
+    average(:age).truncate
+  end
+
 end
